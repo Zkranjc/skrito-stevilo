ff3d423318b598b76e041d55d4537458cb8f9b56
Žan Kranjc <zan.kranjc92@gmail.com>
2017-02-09 13:30:57 +0100

    prva posodobitev

---
+++ .idea/skrito število.py
@@ -0,0 +1,4 @@
+# -*- encoding: utf-8 -*-
+skrito_stevilo = "skrito število štiri"
+print (skrito_stevilo)
+
---
+++ skrita_stevilka.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+skrito_stevilo = 4
+ugani = int(raw_input("(ugani skrito število med 1 in 20) tukaj vnesi številko: "))
+if ugani == skrito_stevilo:
+    print ("Čestitam uganili ste skrito število (4)")
+else:
+    print("Žal to število ni pravilno")
+
+
